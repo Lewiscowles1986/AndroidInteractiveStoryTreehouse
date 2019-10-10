b3eceeaa9f9d2a7d52277b2ffac02c50426cbf4f
Lewis Cowles <lewiscowles@me.com>
2019-10-10 23:15:41 +0100

    Adjust Android API changes
    So it's not just Gradle which breaks builds...
    This might be Java changes
    
    I'm now building with OpenJDK
    
    No idea what I was building with before
    Whatever came with AndroidStudio I guess

--- Interactivestory/app/src/main/java/uk/co/lewiscowles/interactivestory/ui/MainActivity.java
+++ Interactivestory/app/src/main/java/uk/co/lewiscowles/interactivestory/ui/MainActivity.java
@@ -1,7 +1,7 @@
 package uk.co.lewiscowles.interactivestory.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
